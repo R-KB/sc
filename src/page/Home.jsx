44205bc2f7d02adcb124674b5eafab6c25d1ecd1
R-KB <>
2026-03-24 14:29:34 +0900

    homeにリンク追加

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,15 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router";
 
 export default function Home() {
   return (
     <>
       <Container>
         <h2>一覧</h2>
+        <hr />
+        <div className="two-by"><Link to="/edit"><Button variant="info" className="home__btn">編集画面</Button></Link>
+        <Link to="/check"><Button variant="success" className="home__btn">check</Button></Link></div>
+        <hr />
         
       </Container>
     </>
