2cf13fb6dc61f40686222c4707fea5b070876aea
R-KB <>
2026-03-24 11:54:54 +0900

    ジャンルデータ取得チェック w/ tanstack

--- src/component/EditGenre.jsx
+++ src/component/EditGenre.jsx
@@ -1,5 +1,34 @@
+import { Alert, Spinner } from "react-bootstrap";
+import useGetGenre from "../hook/useGetGenre"
+
 export default function EditGenre() {
-  return (
-    <div>EditGenre</div>
-  )
+  const { isLoading, isError, genre } = useGetGenre();
+  if(isError) {
+    return <Alert variant="danger">データ取得エラー</Alert>;
+  }
+
+  if(isLoading) {
+    return (
+      <Alert className="d-flex justify-content-center">
+        <Spinner animation="border" />
+      </Alert>
+    );
+  }
+
+  if(genre.length === 0) {
+    return <Alert variant="info">データがありません</Alert>;
+  }
+
+  return (<>
+    <h3>ジャンル編集</h3>
+    <hr />
+    <ul>
+      {genre.map((genre) => (
+        <li key={genre.id} className="genre__edit--p">
+          {genre.id}. {genre.name}
+        </li>
+      ))}
+    </ul>
+    </>
+  );
 }
